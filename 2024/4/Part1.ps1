425f0a968d5d868022afd130c690f8182991d369
Bas van der Waal <bas_vdwaal@hotmail.com>
2024-12-06 12:28:15 +0100

    Implemented better searching algorithm

--- 2024/4/Part1.ps1
+++ 2024/4/Part1.ps1
@@ -7,91 +7,88 @@ function Convert-TextToGrid {
 
     # Resultaat grid
     $grid = @()
+    $gridHashtable = @{}
 
     for ($y = 0; $y -lt $TextLines.Count; $y++) {
         $line = $TextLines[$y]
-        for ($x = 0; $x -lt $line.Length -1; $x++) {
+        for ($x = 0; $x -lt $line.Length; $x++) {
             $letter = $line[$x]
-            $grid += [PSCustomObject]@{
+            $obj = [PSCustomObject]@{
                 X      = $x
                 Y      = $y
                 Letter = $letter
             }
+            $grid += $obj
+
+            $key = "$x,$y"
+            $gridHashtable[$key] = $obj
         }
     }
 
-    return $grid
+    return $grid, $gridHashtable
 }
 
-function Check-SurroundingCoords {
+$Directions = @(
+    [PSCustomObject]@{ Direction = "Right"; X = 1; Y =  0},
+    [PSCustomObject]@{ Direction = "Left"; X = -1; Y =  0},
+    [PSCustomObject]@{ Direction = "Up"; X = 0; Y =  1},
+    [PSCustomObject]@{ Direction = "Down"; X = 0; Y =  -1},
+    [PSCustomObject]@{ Direction = "TopRight"; X = 1; Y =  1},
+    [PSCustomObject]@{ Direction = "TopLeft"; X = -1; Y =  1},
+    [PSCustomObject]@{ Direction = "BottomRight"; X = 1; Y =  -1},
+    [PSCustomObject]@{ Direction = "BottomLeft"; X = -1; Y =  -1}
+)
+
+$count = 0
+
+Write-Host "Generating grid.."
+$grid, $gridHashtable = Convert-TextToGrid -TextLines (Get-Content $PSScriptRoot\Input.txt)
+
+function Convert-GridToStringArray {
     param (
-        $StartingCoords,
-        $LetterToFind
+        [array]$Grid
     )
 
-    $arr = @()
-    for ($y = $StartingCoords.Y - 1; $y -lt $StartingCoords.Y + 2; $y++)
-    {
-        Write-Host "Checking Y=$y"
-        for ($x = $StartingCoords.X - 1; $x -lt $StartingCoords.X + 2; $x++)
+    # Bepaal de maximale Y-coördinaat om het aantal rijen te weten
+    $maxY = ($Grid | Measure-Object -Property Y -Maximum).Maximum
+
+    # Maak een array voor de output
+    $result = @()
+
+    for ($y = 0; $y -le $maxY; $y++) {
+        # Selecteer alle items in de huidige rij
+        $rij = $Grid | Where-Object { $_.Y -eq $y } | Sort-Object X
+
+        # Maak een string van de letters in de rij
+        $result += ($rij | ForEach-Object { $_.Letter }) -join ''
+    }
+
+    return $result
+}
+# Convert-GridToStringArray -Grid $grid
+
+Write-Host "Starting Search.."
+foreach ($StartPoint in $grid) {
+    if ($StartPoint.Letter -eq 'X') {
+        # Write-Host "Found starting point ($($StartPoint.X),$($StartPoint.Y))"
+        foreach ($Direction in $Directions)
         {
-            Write-Host "Checking X=$X"
-            $Item = $grid | where {$_.X -eq $x -and $_.Y -eq $Y}
-            if ($item -and $Item.Letter -eq $LetterToFind)
+            $Point2 = $gridHashtable["$($StartPoint.X + $Direction.X),$($StartPoint.Y + $Direction.Y)"]
+            if ($Point2 -and $Point2.Letter -eq 'M')
             {
-                $arr += $Item
-                Write-Host "Found Letter $($item.Letter)"
+                $Point3 = $gridHashtable["$($Point2.X + $Direction.X),$($Point2.Y + $Direction.Y)"]
+                if ($Point3 -and $Point3.Letter -eq 'A')
+                {
+                    $Point4 = $gridHashtable["$($Point3.X + $Direction.X),$($Point3.Y + $Direction.Y)"]
+                    if ($Point4 -and $Point4.Letter -eq 'S')
+                    {
+                        # Write-Host "Found XMAS: ($($Startpoint.X),$($Startpoint.Y)) to ($($Point4.X),$($Point4.Y))"
+                        $count++
+                    }
+                }
             }
-        }
-    }
-
-    if (($arr | Measure-Object).Count -gt 0)
-    {
-        return $arr
-    }
-    else
-    {
-        return $false
-    }
-}
-
-$SampleInput = @"
-MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX
-"@ -split "`n"
-
-
-$Script:grid = Convert-TextToGrid -TextLines $SampleInput
-
-foreach ($Item in $grid)
-{
-    if ($Item.Letter -eq 'X' -or $Item.Letter -eq 'S')
-    {
-        if ($Item.Letter -eq 'X') {$WordToFind = 'XMAS'}
-        elseif ($Item.Letter -eq 'S') {$WordToFind = 'SAMX'}
-
-        $LetterToFind = ($word -replace $Item.Letter, '')[0]
-        Write-Host "Found $($item.Letter) on X=$($item.X),Y=$($item.Y), finding $LetterToFind"
-
-        # Check surrounding grid coords for the next letter
-        [array]$res = Check-SurroundingCoords -StartingCoords $Item -LetterToFind $LetterToFind
-        foreach ($seconditem in $res)
-        {
-            Write-Host "Found $($seconditem.Letter) on X=$($seconditem.X),Y=$($seconditem.Y)"
-
-
-
-
-
 
         }
     }
 }
+Write-Host "Count: $count"
