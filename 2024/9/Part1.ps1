7d2d1bf14d4d01ef38b273a3d67d8da94cf8c3d1
Bas van der Waal <basvanderwaal@dsw.nl>
2024-12-09 16:50:37 +0100

    Part 1 optimised

--- 2024/9/Part1.ps1
+++ 2024/9/Part1.ps1
@@ -12,71 +12,51 @@ $PuzzleInput = (Get-Content $PSScriptRoot\Input.txt)
 
 $ExtendedArray = New-Object System.Collections.ArrayList
 $id = 0
+$EmptyChar = [char]"."
 
-For($i = 0;$i -lt $PuzzleInput.Length;$i++)
+For ($i = 0; $i -lt $PuzzleInput.Length; $i++)
 {
     $char = $PuzzleInput[$i]
-    if ($i % 2 -eq 0) 
+    if ($i % 2 -eq 0)
     {
         # Current character indicates file length
-        # $ExtendedArray.Add([string]$id * [int]::Parse($char)) | Out-Null
-
-        # $ExtendedArray.Add(
-        #     [PSCustomObject]@{
-        #         Id = $id
-        #         Length = [int]::Parse($char)
-                
-        #     }
-        # ) | Out-Null
-        
-        For($j = 0;$j -lt [int]::Parse($char);$j++)
+        For ($j = 0; $j -lt [int]::Parse($char); $j++)
         {
-            $ExtendedArray.Add($id) | Out-Null
+            $ExtendedArray.Add($id)
         }
         $id++
     }
     else
     {
         # Current Char indicates free space
-        # $ExtendedArray.Add(
-        #     [PSCustomObject]@{
-        #         Id = "."
-        #         Length = [int]::Parse($char)
-                
-        #     }
-        # ) | Out-Null
-        For($k = 0;$k -lt [int]::Parse($char);$k++)
+        For ($k = 0; $k -lt [int]::Parse($char); $k++)
         {
-            $ExtendedArray.Add(".") | Out-Null
+            $ExtendedArray.Add(".")
         }
     }
 }
-
-# Convert the Array into a single digit array
-# [System.Collections.ArrayList]$ExtendedArray = ($ExtendedArray -join "").ToCharArray()
-# Write-Host ($ExtendedArray -join "")
 
 $UpdatedArrayLength = $ExtendedArray.Count
 for ($i = 0; $i -lt ($UpdatedArrayLength - 1); $i++)
 {
     try
     {
-        While ($ExtendedArray[-1] -eq '.')
+        While ([char]$ExtendedArray[-1] -eq $EmptyChar)
         {
             $ExtendedArray.RemoveAt($ExtendedArray.Count - 1)
         }
 
-        if ($ExtendedArray[$i] -eq '.')
+        if ([char]$ExtendedArray[$i] -eq $EmptyChar)
         {
             $ExtendedArray[$i] = $ExtendedArray[-1]
             $ExtendedArray.RemoveAt($ExtendedArray.Count - 1)
             $UpdatedArrayLength = $ExtendedArray.Count
-            # Write-Host ($ExtendedArray -join "")
         }
     }
     catch
     {
-        $_
+        # Index out of bounds error because we remove dots at the end of the array but still try to iterate over them.
+        break
     }
 
 }
@@ -84,7 +64,6 @@ for ($i = 0; $i -lt ($UpdatedArrayLength - 1); $i++)
 $CheckSum = 0
 for ($i = 0; $i -lt $ExtendedArray.Count; $i++)
 {
-    # Write-Host "$i * $($ExtendedArray[$i]) = $($i * [int]::Parse($ExtendedArray[$i]))"
     $CheckSum += $i * [int]::Parse($ExtendedArray[$i])
 }
 
