3def4d05919845aadbfba3324bbb33c5578f0614
Bas van der Waal <bas_vdwaal@hotmail.com>
2024-12-12 19:06:32 +0100

    Working solution Part 2

--- 2024/12/Part2.ps1
+++ 2024/12/Part2.ps1
@@ -9,7 +9,8 @@ $PuzzleInput = (Get-Content $PSScriptRoot\Input.txt)
 # ======================================================================
 
 #region Functions
-function Convert-TextToGrid {
+function Convert-TextToGrid
+{
     param (
         [string[]]$TextLines
     )
@@ -17,16 +18,17 @@ function Convert-TextToGrid {
     # Resultaat grid
     $Grid = @{}
 
-    for ($y = 0; $y -lt $TextLines.Count; $y++) {
+    for ($y = 0; $y -lt $TextLines.Count; $y++)
+    {
         $line = $TextLines[$y]
-        for ($x = 0; $x -lt $line.Length; $x++) {
+        for ($x = 0; $x -lt $line.Length; $x++)
+        {
             $letter = $line[$x]
             $obj = [PSCustomObject]@{
-                X      = $x
-                Y      = $y
-                Letter = $letter
-                Boundaries = @()
-                $corner = $false
+                X          = $x
+                Y          = $y
+                Letter     = $letter
+                Visited    = $false
             }
             $Grid["$x,$y"] = $obj
         }
@@ -35,23 +37,87 @@ function Convert-TextToGrid {
     return $Grid
 }
 
+
+function Count-Corners
+{
+    param (
+        $Tile
+    )
+
+    if ($Grid["$($Tile.X + 1),$($Tile.Y - 1)"]) { $TopRightTile = $Grid["$($Tile.X + 1),$($Tile.Y - 1)"].Letter }    else { $TopRightTile = "" }
+    if ($Grid["$($Tile.X - 1),$($Tile.Y - 1)"]) { $TopLeftTile = $Grid["$($Tile.X - 1),$($Tile.Y - 1)"].Letter }     else { $TopLeftTile = "" }
+    if ($Grid["$($Tile.X + 1),$($Tile.Y + 1)"]) { $BottomRightTile = $Grid["$($Tile.X + 1),$($Tile.Y + 1)"].Letter } else { $BottomRightTile = "" }
+    if ($Grid["$($Tile.X - 1),$($Tile.Y + 1)"]) { $BottomLeftTile = $Grid["$($Tile.X - 1),$($Tile.Y + 1)"].Letter }  else { $BottomLeftTile = "" }
+    if ($Grid["$($Tile.X + 1),$($Tile.Y)"])     { $RightTile = $Grid["$($Tile.X + 1),$($Tile.Y)"].Letter }           else { $RightTile = "" }
+    if ($Grid["$($Tile.X - 1),$($Tile.Y)"])     { $LeftTile = $Grid["$($Tile.X - 1),$($Tile.Y)"].Letter }            else { $LeftTile = "" }
+    if ($Grid["$($Tile.X),$($Tile.Y - 1)"])     { $TopTile = $Grid["$($Tile.X),$($Tile.Y - 1)"].Letter }             else { $TopTile = "" }
+    if ($Grid["$($Tile.X),$($Tile.Y + 1)"])     { $BottomTile = $Grid["$($Tile.X),$($Tile.Y + 1)"].Letter }          else { $BottomTile = "" }
+
+    $CornerCount = 0
+
+    if ($Tile.X -eq 3 -and $tile.Y -eq 2)
+    {
+        "" | Out-Null
+    }
+
+
+    # Inner corners
+    if ($TopRightTile -ne $Tile.Letter -and $TopTile -eq $Tile.Letter -and $RightTile -eq $Tile.Letter)
+    {
+        $CornerCount++
+    }
+    if ($TopLeftTile -ne $Tile.Letter -and $TopTile -eq $Tile.Letter -and $LeftTile -eq $Tile.Letter)
+    {
+        $CornerCount++
+    }
+    if ($BottomRightTile -ne $Tile.Letter -and $BottomTile -eq $Tile.Letter -and $RightTile -eq $Tile.Letter)
+    {
+        $CornerCount++
+    }
+    if ($BottomLeftTile -ne $Tile.Letter -and $BottomTile -eq $Tile.Letter -and $LeftTile -eq $Tile.Letter)
+    {
+        $CornerCount++
+    }
+
+    # Outer Corners
+    if ($TopTile -ne $Tile.Letter -and $RightTile -ne $Tile.Letter)
+    {
+        $CornerCount++
+    }
+    if ($TopTile -ne $Tile.Letter -and $LeftTile -ne $Tile.Letter)
+    {
+        $CornerCount++
+    }
+    if ($BottomTile -ne $Tile.Letter -and $RightTile -ne $Tile.Letter)
+    {
+        $CornerCount++
+    }
+    if ($BottomTile -ne $Tile.Letter -and $LeftTile -ne $Tile.Letter)
+    {
+        $CornerCount++
+    }
+
+    return $CornerCount
+}
+
+
 #Endregion Functions
 
 $Directions = @(
-    [PSCustomObject]@{ Direction = "Right"; X = 1; Y =  0},
-    [PSCustomObject]@{ Direction = "Left"; X = -1; Y =  0},
-    [PSCustomObject]@{ Direction = "Up"; X = 0; Y =  -1},
-    [PSCustomObject]@{ Direction = "Down"; X = 0; Y =  1}
+    [PSCustomObject]@{ Direction = "Right"; X = 1; Y = 0 },
+    [PSCustomObject]@{ Direction = "Left"; X = -1; Y = 0 },
+    [PSCustomObject]@{ Direction = "Up"; X = 0; Y = -1 },
+    [PSCustomObject]@{ Direction = "Down"; X = 0; Y = 1 }
 )
 
 $Grid = Convert-TextToGrid -TextLines $PuzzleInput
 $Plots = @{}
 $PlotID = 0
-$Visited = @()
+$Corners = @{}
 
 foreach ($Tile in $Grid.Values)
 {
-    if ($tile -in $Visited)
+    if ($tile.Visited)
     {
         continue
     }
@@ -76,7 +142,7 @@ foreach ($Tile in $Grid.Values)
             {
                 if ($NextTile.Letter -eq $Tile.Letter)
                 {
-                    if ($Visited -contains $NextTile)
+                    if ($NextTile.Visited)
                     {
                         # Write-Host "Tile ($($NextTile.X),$($NextTile.y)) already visited"
                         continue
@@ -90,99 +156,33 @@ foreach ($Tile in $Grid.Values)
                         $Queue.Enqueue($NextTile)
                     }
                 }
-                else
-                {
-                    # Write-Host "Tile ($($NextTile.X),$($NextTile.y)) is a different type."
-                    $tile.Boundaries += $Direction
-                }
-            }
-            else
-            {
-                # Write-Host "Outside region"
-                $tile.Boundaries += $Direction
             }
         }
-        $Visited += $Tile
-    }
 
+        If ($Corners[$PlotID])
+        {
+            $Corners[$PlotID] += Count-Corners -Tile $Tile
+        }
+        else
+        {
+            $Corners[$PlotID] = Count-Corners -Tile $Tile   
+        }
+
+        $Tile.Visited = $true
+    }
     $PlotID++
-    
 }
 
-
-$Directions = @(
-    [PSCustomObject]@{ Direction = "TopRight"; X = 1; Y =  1},
-    [PSCustomObject]@{ Direction = "TopLeft"; X = -1; Y =  1},
-    [PSCustomObject]@{ Direction = "BottomRight"; X = 1; Y =  -1},
-    [PSCustomObject]@{ Direction = "BottomLeft"; X = -1; Y =  -1}
-)
-
+# Write-Host "Corners:"
+# $Corners
+# Write-Host ""
+# Write-Host "Plots:"
+# $Plots
 
 $TotalCost = 0
 Foreach ($PlotID in $Plots.Keys)
 {
-    $Plot = $Plots[$PlotID]
-    $TotalBoundaries = 0
-    foreach ($Tile in $Plot)
-    {
-        $TopRightTile = $Grid["$($Tile.X + 1),$($Tile.Y + 1)"]
-        $TopLeftTile = $Grid["$($Tile.X - 1),$($Tile.Y + 1)"]
-        $BottomRightTile = $Grid["$($Tile.X + 1),$($Tile.Y - 1)"]
-        $BottomLeftTile = $Grid["$($Tile.X - 1),$($Tile.Y - 1)"]
-        
-        
-        # Topright inner corner
-        if ($TopRightTile.Letter -ne $Tile.Letter -and ($TopLeftTile.Letter -eq $Tile.Letter -or $BottomRightTile.Letter -eq $Tile.Letter))
-        {
-            $Tile.Corner = $true
-        }
-        # Topleft inner corner
-        elseif ($TopLeftTile.Letter -ne $Tile.Letter -and ($TopRightTile.Letter -eq $Tile.Letter -or $BottomLeftTile.Letter -eq $Tile.Letter))
-        {
-            $Tile.Corner = $true
-        }
-        # Bottomright inner corner
-        elseif ($BottomRightTile.Letter -ne $Tile.Letter -and ($BottomLeftTile.Letter -eq $Tile.Letter -or $TopRightTile.Letter -eq $Tile.Letter))
-        {
-            $Tile.Corner = $true
-        }
-        # Topleft inner corner
-        elseif ($TopLeftTile.Letter -ne $Tile.Letter -and ($TopRightTile.Letter -eq $Tile.Letter -or $BottomLeftTile.Letter -eq $Tile.Letter))
-        {
-            $Tile.Corner = $true
-        }
-        # Topright outer corner
-        elseif ($TopRightTile.Letter -ne $Tile.Letter -and ($TopLeftTile.Letter -eq $Tile.Letter -or $BottomRightTile.Letter -eq $Tile.Letter))
-        {
-            $Tile.Corner = $true
-        }
-        # Topleft inner corner
-        elseif ($TopLeftTile.Letter -ne $Tile.Letter -and ($TopRightTile.Letter -eq $Tile.Letter -or $BottomLeftTile.Letter -eq $Tile.Letter))
-        {
-            $Tile.Corner = $true
-        }
-        # Bottomright inner corner
-        elseif ($BottomRightTile.Letter -ne $Tile.Letter -and ($BottomLeftTile.Letter -eq $Tile.Letter -or $TopRightTile.Letter -eq $Tile.Letter))
-        {
-            $Tile.Corner = $true
-        }
-        # Topleft inner corner
-        elseif ($TopLeftTile.Letter -ne $Tile.Letter -and ($TopRightTile.Letter -eq $Tile.Letter -or $BottomLeftTile.Letter -eq $Tile.Letter))
-        {
-            $Tile.Corner = $true
-        }
-
-
-
-        
-    
-    
-    }
-
-
-
-    $TotalCost += $TotalBoundaries * $Plot.Count
-    # Write-Host "$PlotID - $($TotalBoundaries * $Plot.Count)"
+    $TotalCost += $Corners[$PlotID] * $Plots[$PlotID].Count
 }
 
 Write-Host "Total Cost: $TotalCost" 
@@ -190,3 +190,4 @@ Write-Host "Total Cost: $TotalCost"
 # ======================================================================
 
 Write-Host "Runtime: $((Get-Date) - $StartTime)"
+# Runtime: 00:00:01.7874646
