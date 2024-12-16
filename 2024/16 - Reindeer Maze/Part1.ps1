55fde7628460101e449b2ab5d40648a3be2191b6
Bas van der Waal <basvanderwaal@dsw.nl>
2024-12-16 17:13:05 +0100

    Working solution Part 1

--- 2024/16 - Reindeer Maze/Part1.ps1
+++ 2024/16 - Reindeer Maze/Part1.ps1
@@ -27,6 +27,7 @@ function Convert-TextToGrid {
                 Visited = $false
                 String = "$x,$y"
                 Cost = -1
+                Direction = ""
             }
             $Grid["$x,$y"] = $obj
         }
@@ -110,7 +111,7 @@ while ($Stack.Count -gt 0)
 }
 #>
 
-<# Dijkstra algorithm
+<# A* algorithm
 $Queue = New-Object 'System.Collections.Generic.PriorityQueue[PSCustomObject, int]'
 $Queue.Enqueue( $StartTile, 0 )
 $Came_from = @{}
@@ -155,24 +156,22 @@ while ($Queue.Count -gt 0)
 }
 #>
 
-# A* algorithm
+# Dijkstra algorithm
 $Queue = New-Object 'System.Collections.Generic.PriorityQueue[PSCustomObject, int]'
 $Queue.Enqueue( $StartTile, 0 )
 $Came_from = @{}
-# $Cost_so_far = @{}
 $Came_from[$StartTile.String] = $null
-# $cost_so_far[$StartTile.String] = 0
 $StartTile.cost = 0
 
 # Starting direction is east
-$PD = $Directions | where Direction -eq "East"
+$StartTile.Direction = $Directions | where Direction -eq "East" | select -ExpandProperty Direction
 
 while ($Queue.Count -gt 0)
 {
     $Tile = $Queue.Dequeue()
     if ($Tile.Letter -eq "E")
     {
-        Continue
+        break
     }
 
     foreach ($D in $Directions)
@@ -180,15 +179,13 @@ while ($Queue.Count -gt 0)
         $NextTile = $Grid["$($Tile.X + $D.X),$($Tile.Y + $D.Y)"]
         if ($NextTile.Letter -eq "#") { continue }
 
-        $ExtraCost = $D -ne $PD ? 1001 : 1  # Ternary checks!
+        $ExtraCost = $D.Direction -ne $Tile.Direction ? 1001 : 1  # Ternary checks!
 
-        # $NewCost = $Cost_so_far[$Tile.String] + $ExtraCost
         $NewCost = $Tile.Cost + $ExtraCost
 
-        # if (!($Cost_so_far[$NextTile.String]) -or $NewCost -lt $Cost_so_far[$NextTile.String])
         if ($NextTile.Cost -eq -1 -or $NewCost -lt $NextTile.Cost)
         {
-            # $Cost_so_far[$NextTile.String] = $NewCost
+            $NextTile.Direction = $D.Direction
             $NextTile.Cost = $NewCost
             $priority = $NewCost
             $Queue.Enqueue($NextTile, $priority)
@@ -196,33 +193,37 @@ while ($Queue.Count -gt 0)
         }
     }
     
-    ############  Issue here!
-    $PD = $D
     # Write-GridToScreen $Grid
     # $null = Read-host " "
 }
 #>
 
+<#
 $path = New-Object System.Collections.ArrayList
-
 while ($Tile -ne $StartTile) {
     [void]$path.Add($Tile)
     $Tile = $came_from[$tile.String]
 }
 # [void]$path.Add($StartTile)
 $path.Reverse()
+$path.Remove($EndTile) | Out-Null # remove end tile from array
 
 foreach ($tile in $path)
 {
     $tile.Letter = "X"
-    Write-GridToScreen $Grid
-    Write-Host $Tile.Cost
-    $null = Read-host " "
+    
+    # Write-Host $Tile.Cost
+    # $null = Read-host " "
+    # Start-Sleep -Milliseconds 20
 }
 
-Write-Host "Cost: $($Cost_so_far[$EndTile.String])"
+Write-GridToScreen $Grid
+
+#>
+Write-Host "Cost: $($EndTile.Cost)"
 
 # ======================================================================
 # ======================================================================
 
 Write-Host "Runtime: $((Get-Date) - $StartTime)"
+# Runtime: 00:00:01.2384169
