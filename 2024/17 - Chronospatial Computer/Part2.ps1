0b7ae5f16ec534c59b2422fff343dceac7cd5219
Bas van der Waal <bas_vdwaal@hotmail.com>
2024-12-19 08:16:02 +0100

    Wip

--- 2024/17 - Chronospatial Computer/Part2.ps1
+++ 2024/17 - Chronospatial Computer/Part2.ps1
@@ -1,110 +1,166 @@
-
+# https://github.com/hyperneutrino/advent-of-code/blob/main/2024/day17p2.py
 Set-StrictMode -Version latest
 $StartTime = Get-Date
 
-$PuzzleInput = (Get-Content $PSScriptRoot\Input.txt)
-
 # ======================================================================
 # ======================================================================
 
-#region Functions
+<#
+2,4 -> $RegB = $RegA % 8
+1,3 -> $RegB = $RegB -bxor 3 
+7,5 -> $RegC = [math]::Floor( $RegA / [math]::Pow(2, $RegB) )
+1,5 -> $RegB = $RegB -bxor 5
+0,3 -> $RegA = [math]::Floor( $RegA / 8 )
+4,1 -> $RegB = $RegB -bxor $RegC
+5,5 -> $out += $RegB % 8
+3,0 -> Loop while A != 0
+#>
 
-$Script:RegA = 117440
-$Script:RegB = 0
-$Script:RegC = 0
-$Script:Out = New-Object System.Collections.ArrayList
+# Program: 2,4,1,3,7,5,1,5,0,3,4,1,5,5,3,0
+#          
 
-$Instructions = 0,3,5,4,3,0
+# 8, 64, 512, 4096, 32768, 262144, 2097152, 16777216 -> (0-8) * 8 = 6,6,6,6,6, etc gevolgd door 2 eindcijfers
+# 16777216 -> 6,6,6,6,6,6,6,7,7, 16777217 -> 7,6,6,6,6,6,6,7,7, 16777218 -> 5,6,6,6,6,6,6,7,7
 
 
-function Get-ComboOperand ($Operand)
-{
-    switch ($Operand)
+# for ([int64]$i = 64; $i -le 127; $i++) 
+# {
+#     $RegA = $i
+#     $RegB = 0
+#     $RegC = 0
+#     $out = @()
+
+#     if($i % 100000 -eq 0)
+#     {
+#         Write-Host $i
+#     }
+
+#     while ($RegA -ne 0)
+#     {
+#         $RegB = $RegA % 8 # 1 cijfer
+#         $RegB = $RegB -bxor 3 # 0:3, 1:2, 2:1, 3:0, 4:7, 5:6, 6:5, 7:4
+#         $RegC = [math]::Floor( $RegA / [math]::Pow(2, $RegB) ) # RegA / 1|2|4|8|16|32|64|128 -> Groot nummber
+#         $RegB = $RegB -bxor 5 # 0:5, 1:4, 2:7, 3:6, 4:1, 5:0, 6:3, 7:2
+#         $RegA = [math]::Floor( $RegA / 8 ) # 
+#         $RegB = $RegB -bxor $RegC
+#         $out += $RegB % 8  # 1 cijfer
+#     }
+    
+#     Write-Host "$i : $($out -join ",")"
+    
+#     # if ($out -join "," -eq "2,4,1,3,7,5,1,5,0,3,4,1,5,5,3,0")
+#     # {
+#     #     Write-Host "Start value for A: $i"
+#     #     break
+#     # }
+# }
+
+
+function calculate ($a, $b) {
+    $RegA = $a
+    $RegB = $b
+    $RegC = 0
+    $out = @()
+
+    while ($RegA -ne 0)
     {
-        0 { return 0 }
-        1 { return 1 }
-        2 { return 2 }
-        3 { return 3 }
-        4 { return $Script:RegA }
-        5 { return $Script:RegB }
-        6 { return $Script:RegC }
-        7 { Write-Host "Should not appear!"; return $null }
-        Default { Write-Host "Unknown Operand!"; return $null }
+        # $RegB = $RegA % 8 # 1 cijfer
+        $RegB = $RegB -bxor 3 # 0:3, 1:2, 2:1, 3:0, 4:7, 5:6, 6:5, 7:4
+        $RegC = [math]::Floor( $RegA / [math]::Pow(2, $RegB) ) # RegA / 1|2|4|8|16|32|64|128 -> Groot nummber
+        $RegB = $RegB -bxor 5 # 0:5, 1:4, 2:7, 3:6, 4:1, 5:0, 6:3, 7:2
+        $RegA = [math]::Floor( $RegA / 8 ) # 
+        $RegB = $RegB -bxor $RegC
+        $out += $RegB % 8  # 1 cijfer
     }
+    
+    Write-Host "$a, $b : $($out -join ",")"
 }
 
+# function combo ($Operand)
+# {
+#     switch ($Operand)
+#     {
+#         0 { return 0 }
+#         1 { return 1 }
+#         2 { return 2 }
+#         3 { return 3 }
+#         4 { return $Script:RegA }
+#         5 { return $Script:RegB }
+#         6 { return $Script:RegC }
+#     }
+# }
+$program = 2,4,1,3,7,5,1,5,0,3,4,1,5,5,3,0
 
-function adv ($Operand)  # Opcode 0 - Devide RegA by 2 ^ combo Operand
+
+function find ($target, $ans)
 {
-    $Script:RegA = [math]::Floor( $Script:RegA / [math]::Pow(2, $Operand) )
-}
+    Write-Host $target.Length
+    if (! $Target) {return $ans}
+    foreach ($t in @(0..8)){
+        $A = ([math]::Floor( $ans / [math]::Pow(2,-$t) )) + $t
+        $B = 0
+        $C = 0
+        $Output = $null
+        
+        function combo ($Operand) {
+            switch ($Operand)
+            {
+                0 { return 0 }
+                1 { return 1 }
+                2 { return 2 }
+                3 { return 3 }
+                4 { return $A }
+                5 { return $B }
+                6 { return $C }
+            }
+        }
 
-function bxl ($Operand)  # Opcode 1 - bitwise XOR of RegB and Literal Operand
-{
-    $Script:RegB = $Script:RegB -bxor $Operand
-}
-
-function bst ($Operand)  # Opcode 2 - Combo operand % 8, keep last 3 bits
-{
-    $Script:RegB = $Operand % 8
-
-    #TODO fix 3 bits part
-}
-
-function jnz ($Operand, $index)  # Opcode 3 - if not 0, jump
-{
-    if ($Script:RegA -eq 0) { return ($index += 2) }
-    return $Operand
-}
-
-function bxc ()  # Opcode 4 - bitwise XOR of RegB and RegC
-{
-    $Script:RegB = $Script:RegB -bxor $Script:RegC
-}
-
-function out ($Operand)  # Opcode 5 - Combo operand % & -> output (comma separated)
-{
-    $script:out += $Operand % 8
-}
-
-function bdv ($Operand)  # Opcode 6 - Devide RegA by 2 ^ combo Operand and stored in RegB
-{
-    $Script:RegB = [math]::Floor( $Script:RegA / [math]::Pow(2, $Operand) )
-}
-
-function cdv ($Operand)  # Opcode 7 - Devide RegA by 2 ^ combo Operand and stored in RegC
-{
-    $Script:RegC = [math]::Floor( $Script:RegA / [math]::Pow(2, $Operand) )
-}
-
-#Endregion Functions
-
-
-for ($i = 0; $i -lt $Instructions.Count; ) 
-{
-    try
-    {
-        $instr = $Instructions[$i]
-        $Operand = $Instructions[$i + 1]
-        switch ($instr)
+        for ($pointer = 0; $pointer -lt ($program.count - 2); $pointer += 2)
         {
-            0 { adv (Get-ComboOperand $Operand); $i += 2 }
-            1 { bxl $Operand; $i += 2 }
-            2 { bst (Get-ComboOperand $Operand); $i += 2 }
-            3 { $i = jnz $Operand $i; }
-            4 { bxc; $i += 2 }
-            5 { out (Get-ComboOperand $Operand); $i += 2 }
-            6 { bdv (Get-ComboOperand $Operand); $i += 2 }
-            7 { cdv (Get-ComboOperand $Operand); $i += 2 }
+            try
+            {
+                $ins = $program[$pointer]
+                $operand = $program[($pointer + 1)]
+
+                if ($ins -eq 0) {}
+                elseif ($ins -eq 1) {
+                    $b = $b -bxor $operand
+                }
+                elseif ($ins -eq 2) {
+                    $b = (combo $operand) -band (8 - 1)
+                    # (combo $operand) % 8
+                    # (combo $operand) -band (8 - 1)
+                }
+                elseif ($ins -eq 4) {
+                    $b = $b -bxor $C
+                }
+                elseif ($ins -eq 5) {
+                    $Output = combo($operand) -band (8 - 1)
+                }
+                elseif ($ins -eq 6) {
+                    $B = [math]::Floor( $A / [math]::Pow(2, (combo $operand ) ) )
+                }
+                elseif ($ins -eq 7) {
+                    $C = [math]::Floor( $A / [math]::Pow(2, (combo $operand ) ) )
+                }
+
+                if ($Output -eq $target[-1])
+                {
+                    $sub = find -target $target[0..$($target.Length-2)] -ans $A
+                    if (! $sub) {continue}
+                    return $sub
+                }
+            }
+            catch
+            {
+                $_
+            }
         }
     }
-    catch
-    {
-        $_
-    }
 }
 
-$Script:Out -join ","
+
+find -target $program -ans 0
 
 
 
