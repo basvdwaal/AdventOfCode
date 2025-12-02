607c630230b6dab71ff841bdc9d25d435029cdf8
Bas van der Waal <basvanderwaal@dsw.nl>
2025-12-02 18:00:11 +0100

    Solution AoC day 2

---
+++ 2025/Day2 - Gift Shop/Part1.ps1
@@ -0,0 +1,40 @@
+
+Set-StrictMode -Version latest
+
+# $PuzzleInput = (Get-Content $PSScriptRoot\sample.txt)
+$PuzzleInput = (Get-Content $PSScriptRoot\Input.txt)
+
+$StartTime = Get-Date
+# ======================================================================
+# ======================================================================
+
+#region Functions
+
+
+
+#Endregion Functions
+$regex = [regex]"^(?<First>\d+)\k<First>$"
+$Total = 0
+
+foreach($Range in $PuzzleInput -split ",")
+{
+    [int64]$Begin, [int64]$End = ($Range -split "-")
+
+    for ($number = $begin; $number -le $end; $number++)
+    {
+        if ($number -match $regex)
+        {
+            # Write-Host "$number is fake"
+            $Total += $number
+        }
+    }
+}
+
+write-host $Total
+
+
+# ======================================================================
+# ======================================================================
+
+Write-Host "Runtime: $((Get-Date) - $StartTime)"
+# Runtime: 00:00:05.4600889
