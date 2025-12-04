b9e35ff3e66a956c427702b83b4bc4518d2a5248
Bas van der Waal <bas_vdwaal@hotmail.com>
2025-12-04 21:50:06 +0100

    Solutiopn AoC day 3 part 2

--- 2025/Day3 - Lobby/Part2.ps1
+++ 2025/Day3 - Lobby/Part2.ps1
@@ -1,8 +1,8 @@
 
 Set-StrictMode -Version latest
 
-$PuzzleInput = (Get-Content $PSScriptRoot\sample.txt)
-# $PuzzleInput = (Get-Content $PSScriptRoot\Input.txt)
+# $PuzzleInput = (Get-Content $PSScriptRoot\sample.txt)
+$PuzzleInput = (Get-Content $PSScriptRoot\Input.txt)
 
 $StartTime = Get-Date
 # ======================================================================
@@ -14,10 +14,42 @@ $StartTime = Get-Date
 
 #Endregion Functions
 
+$total = 0
 
+foreach ($Sequence in $PuzzleInput)
+{
+    $Numbers = [int[]]($Sequence.ToCharArray() | % { $_ - 48})
+    
+    $BatteryArr = @()
+    $LastNumberIndex = -1
+    $NumbersNeeded = 12
+    while ($BatteryArr.Length -lt 12)
+    {
+        $BiggestNumber = 0
+        $BiggestNumberIndex = 0
+        for ($i = $LastNumberIndex + 1; $i -le $Numbers.Count - $NumbersNeeded ; $i++)
+        {
+            $Number = $Numbers[$i]
+            if ($Number -gt $BiggestNumber) { 
+                $BiggestNumber = $Number
+                $BiggestNumberIndex = $i
+            }
+        }
+
+        $LastNumberIndex = $BiggestNumberIndex
+        $BatteryArr += $BiggestNumber
+        $NumbersNeeded--
+    }
+    
+    # Write-Host "BiggestPair: $($BatteryArr -join '')"
+    $total += [Int64]($BatteryArr -join "")
+}
+
+Write-Host "Total: $total"
 
 
 # ======================================================================
 # ======================================================================
 
 Write-Host "Runtime: $((Get-Date) - $StartTime)"
+# Runtime: 00:00:00.2697804
